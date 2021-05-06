510da14cc9144bf2dec395f9ed226ad82af7bef0
Your Name <you@example.com>
2021-05-06 16:56:45 +0300

    half input

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('User Inputs...'),
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text('Add Place'),
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).accentColor,
+                elevation: 0,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ))
+        ],
+      ),
+    );
+  }
+}
