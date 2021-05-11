dbcc488b2f15905040b0a95f7794e266a5302d5a
o_dubrovin <interrogatus@gmail.com>
2021-05-11 21:33:50 +0300

    add location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../helpers/location_helper.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +9,16 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+        latitude: locData.latitude, longitude: locData.longitude);
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +42,7 @@ class _LocationInputState extends State<LocationInput> {
                   )),
         Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
               icon: Icon(Icons.location_on),
               label: Text('Current Location'),
               style: TextButton.styleFrom(
